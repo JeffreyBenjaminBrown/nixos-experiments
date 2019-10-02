ac5d9d49679e4f686b7534a707cb01efa47d4cc3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-01 21:04:48 -0500

    broken somehow

--- configuration.nix
+++ configuration.nix
@@ -1,44 +1,6 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
-
-  # to read ExFat, for Borg
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.exfat-nofuse ];
-  
-  networking.hostName = "jbb-dell"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # internationalisation properties
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Bogota";
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     tree
     networkmanager
@@ -55,48 +17,94 @@
     firefox
     brave
     docker
+    dockerTools
     python
     python3
     stack
   ];
   nixpkgs.config.allowUnfree = true; # for Chrome
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
+  # Networking
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    networking.hostName = "jbb-dell"; # Define your hostname.
+    networking.networkmanager.enable = true;
+  
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+  
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Devices
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+    # to read ExFat (which the blue matchbox uses)
+    boot.extraModulePackages = [
+      config.boot.kernelPackages.exfat-nofuse ];
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+  
+    # Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
+  
+    # Enable touchpad support.
+    services.xserver.libinput.enable = true;
 
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
 
-  # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # UI
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+    services.xserver.layout = "us";
+    services.xserver.xkbOptions = "eurosign:e";
+  
+    # Enable the KDE Desktop Environment.
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+
+  ####
+  #### Below: Unlikely to change much
+  ####
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Boot loader
+    boot.loader.grub.enable = true;
+    boot.loader.grub.version = 2;
+    boot.loader.grub.device = "/dev/sda";
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "America/Bogota";
+
+  # User accounts.
+  # Don't forget to set a password with ‘passwd’.
   users.users.jeff = {
     isNormalUser = true;
     extraGroups = [
