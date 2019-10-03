3519cd24b553179c8e24b67e588aee48d48cacf7
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-10-03 10:39:42 -0500

    internationalization (i18n.extraLocaleSettings): mix English and Spanish

--- configuration.nix
+++ configuration.nix
@@ -35,19 +35,19 @@
 
     networking.hostName = "jbb-dell"; # Define your hostname.
     networking.networkmanager.enable = true;
-  
+
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  
+
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
     # programs.mtr.enable = true;
     # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-  
+
     # Enable the OpenSSH daemon.
     # services.openssh.enable = true;
-  
+
     # Open ports in the firewall.
     # networking.firewall.allowedTCPPorts = [ ... ];
     # networking.firewall.allowedUDPPorts = [ ... ];
@@ -63,11 +63,11 @@
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
-  
+
     # Enable sound.
     sound.enable = true;
     hardware.pulseaudio.enable = true;
-  
+
     # Enable touchpad support.
     services.xserver.libinput.enable = true;
 
@@ -78,7 +78,7 @@
     services.xserver.enable = true;
     services.xserver.layout = "us";
     services.xserver.xkbOptions = "eurosign:e";
-  
+
     # Enable the KDE Desktop Environment.
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
@@ -101,7 +101,33 @@
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
+    defaultLocale = "en_US.UTF-8"; # I've tried this both as
+      # English and Spanish. Presumably the next option,
+      # extraLocaleSettings, is more important.
+    extraLocaleSettings = {
+      # These are copied near-verbatim from my work system
+      # (KUbuntu 18.04), except with more quotation marks
+      # (it used them for English but not Spanish),
+      # and space around the equals signs.
+      # Most of them are Colombian Spanish; exceptions are noted.
+      LANG              = "en_US.UTF-8"; # English
+      # LANGUAGE=   # The Ubuntu system shows "LANGUAGE="
+                    # when I evaluate `locale`.
+      LC_CTYPE          = "en_US.UTF-8"; # English
+      LC_NUMERIC        = "es_CO.UTF-8";
+      LC_TIME           = "es_CO.UTF-8";
+      LC_COLLATE        = "en_US.UTF-8"; # English
+      LC_MONETARY       = "es_CO.UTF-8";
+      LC_MESSAGES       = "en_US.UTF-8"; # English
+      LC_PAPER          = "es_CO.UTF-8";
+      LC_NAME           = "es_CO.UTF-8";
+      LC_ADDRESS        = "es_CO.UTF-8";
+      LC_TELEPHONE      = "es_CO.UTF-8";
+      LC_MEASUREMENT    = "es_CO.UTF-8";
+      LC_IDENTIFICATION = "es_CO.UTF-8";
+      # LC_ALL = # The Ubuntu system shows "LANGUAGE="
+                 # when I evaluate `locale`.
+    };
   };
 
   time.timeZone = "America/Bogota";
