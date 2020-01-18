9118f7cde42a9b59695b27dede3c1ac7872a94d9
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-17 20:06:40 -0500

    WORKING:   jackd (through Qjackctl)   musnix STILL NOT WORKING:   realtime kernel

--- audio.nix
+++ audio.nix
@@ -1,6 +1,10 @@
 # This is based on (and minus my username, a strict subset of)
-# magnetophon's commonRT.nix and music.nix files:
+# these files from magnetophon's config:
+  # https://github.com/magnetophon/nixosConfig/blob/master/music.nix
   # https://github.com/magnetophon/nixosConfig/blob/master/commonRT.nix
+  # https://github.com/magnetophon/nixosConfig/blob/master/machines/mixos/default.nix
+#
+# Some parts of this file is magic to me. Those are marked "magic".
 
 {config, lib, pkgs, options, modulesPath}:
 
@@ -10,50 +14,36 @@ with pkgs; {
   # services.jack.jackd.enable = true; # note: also mentioned in the
     # big commented-out section below about JACK, from the NixOS WIKI.
 
-  nixpkgs.config.packageOverrides = pkgs: rec {
-    qjackctl = pkgs.stdenv.lib.overrideDerivation pkgs.qjackctl (
-      oldAttrs: { configureFlags =
-                    # "fix bug for remote running" (says magnetophon's config).
-                    # Allows you to run multiple instances; probably unnecessary.
-                    "--enable-jack-version --disable-xunique";
-                }); };
-
+  # magic to me
   security.sudo.extraConfig = ''
     jeff  ALL=(ALL) NOPASSWD: ${pkgs.systemd}/bin/systemctl
     '';
 
-  # The Musnix RT kernel is currently broken.
-  # Musnix provides a few more things,
-  # but magnetophon suggests trying without first.
-  #
-  # PITFALL: magnetophon notes that one should set
-    # soundcardPciId and rtirq.nameList
-    # I hope I have in fact done that.
-    # I'm assuming he means the so-named musnix options.
-    # PITFALL: In the case of rtirq.nameList, I just copied what he uses;
-    # that could be wrong.
-  #
-  #  imports = [ /home/jeff/nix/musnix ];
-  #  musnix = {
-  #    enable = true;
-  #    alsaSeq.enable = true;
-  #    soundcardPciId = "00:1f.3";
-  #      # PITFALL: This is the id of the built-in soundcard.
-  #      # When I start using the external one, change it.
-  #      # Find it with `lspci | grep -i audio` (per the musnix readme).
-  #    kernel.optimize = true;
-  #    kernel.realtime = true;
-  #     kernel.packages = pkgs.linuxPackages_4_19_rt;
-  #       # PITFALL: magnetophon specifies this. I don't know why.
-  #       # When I tried the default (5_0_rt), NixOS couldn't download
-  #       # http://www.rncbc.org/archive/rtirq-20190129.tar.gz,
-  #       # so now I'm trying this.
-  #    rtirq.enable = true;
-  #    rtirq.nameList = "rtc0 snd";
-  #      # PITFALL: copied with no idea what I'm doing. from
-  #      # https://github.com/magnetophon/nixosConfig/blob/master/machines/mixos/default.nix
-  #      # Running `nixos-option musnix.rtirq.nameList` reveals these other
-  #      # potential candidates: snd usb i8042.
-  #    das_watchdog.enable = true;
-  #  };
+  imports = [ /home/jeff/nix/musnix ];
+  musnix = {
+    enable = true;
+    alsaSeq.enable = false;
+
+    # Find this value with `lspci | grep -i audio` (per the musnix readme).
+    # PITFALL: This is the id of the built-in soundcard.
+    #   When I start using the external one, change it.
+    soundcardPciId = "00:1f.3";
+
+    # If I build with either of these, I get a PREEMPT error, much like
+    #   https://github.com/musnix/musnix/issues/100
+    # kernel.realtime = true;
+    # kernel.optimize = true;
+
+    # das_watchdog.enable = true;
+      # I don't think this does anything without the realtime kernel.
+
+    # magic to me
+    rtirq = {
+      # highList = "snd_hrtimer";
+      resetAll = 1;
+      prioLow = 0;
+      enable = true;
+      nameList = "rtc0 snd";
+    };
+  };
 }
