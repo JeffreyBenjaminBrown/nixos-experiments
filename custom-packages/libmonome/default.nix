0abed44c904b5b20f24166c3aed08a351279156a
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-19 13:11:29 -0500

    build libmonome!

--- custom-packages/libmonome/default.nix
+++ custom-packages/libmonome/default.nix
@@ -1,19 +1,59 @@
-{...}:
+# Apes this file from the nixpkgs repo:
+# pkgs/tools/networking/weighttp/default.nix
 
-with (import <nixpkgs> {});
-derivation {
+# PITFALL: This won't build from here, but it will from nixpkgs.
+# I put it at /pkgs/development/libraries/libmonome,
+# and added the line
+#     libmonome = callPackage ../development/libraries/libmonome { };
+# to
+#     ~/nix/nixpkgs/pkgs/top-level/all-packages.nix
+# right after liblo (arbitrary but kind of natural).
+# Then from the top of nixpkgs, run any of these:
+#   `nix-build -A libmonome`
+#   `nix-env -f . -iA libmonome`.
+
+# PROBLEM: When the libmonome repo is updated, this will break.
+# Will need to change the `sha256` key.
+
+# PROBLEM: Does not seem to have permission to access the monome without sudo:
+# [jeff@jbb-dell:~/nix/nixpkgs]$ monomeserial
+# libmonome: could not open monome device: Permission denied
+# failed to open /dev/ttyUSB0
+# [jeff@jbb-dell:~/nix/nixpkgs]$ sudo monomeserial
+# [sudo] password for jeff:
+# monomeserial version 1.4.2, yay!
+# initialized device m0000102 (monome 256) at /dev/ttyUSB0, which is 16x16 using proto
+#  mext
+# running with prefix /monome
+# ^C
+# [jeff@jbb-dell:~/nix/nixpkgs]$
+
+# MAYBE A PROBLEM: I've got liblo, not liblo-dev, installed.
+# But maybe that distinction matters on Debian and not my system.
+# When I view the Debian package:
+#   https://packages.debian.org/jessie/liblo-dev
+# it looks like all it contains is liblo 7 or 0.28-3:
+#   dep: liblo7 (= 0.28-3)
+# The "liblo" line in my packages.nix should be installing liblo 0.29:
+#   https://nixos.org/nixos/packages.html?channel=nixos-19.03&query=liblo
+
+
+{ stdenv, libudev, liblo, fetchgit, python3, wafHook}:
+
+stdenv.mkDerivation rec {
   name = "libmonome";
-  builder = "${bash}/bin/bash";
-  args = [ ./builder.sh ];
-  buildInputs = [ git
-                  coreutils
-                  liblo
-                  python2
-                ];
-  # I would like to use fetchGit but haven't gotten it to work.
-  # src = fetchGit {
-  #   url = "https://github.com/monome/libmonome.git";
-  # };
-  repo = ./libmonome;
-  system = builtins.currentSystem;
+  version = "1.4.2";
+
+  src = fetchgit {
+    url = https://github.com/monome/libmonome.git;
+    rev = "refs/heads/master";
+    sha256 = "0a23w8qb60vbxrpbzpdh9h5gh05bcb1g94qz77zwavivrj2kjg1s";
+  };
+
+  nativeBuildInputs = [ wafHook ];
+  buildInputs = [
+    liblo
+    libudev
+    python3
+  ];
 }
