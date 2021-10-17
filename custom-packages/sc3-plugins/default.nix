4cc4934ed5b9d0020851950dfebda02bf6b93acb
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-10-16 20:19:45 -0500

    sc3-plugins!

---
+++ custom-packages/sc3-plugins/default.nix
@@ -0,0 +1,29 @@
+# Based on
+# https://gist.github.com/gosub/a42e265ec38d9df203d6
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+
+  name = "sc3-plugins-3.11.1";
+
+  src = fetchgit {
+    url = "https://github.com/supercollider/sc3-plugins";
+    rev = "209cf4ffdcc9181b37aedbf4902e4b4d4090d505"; # This is an ordinary git commit hash, the one corresponding to tag "Version-3.11.1".
+    sha256 = "1cy7g2mvmikml4dg6v4fzw6qr2yv9c94531iwxp501fr9j6z5jh8";
+      # To determine this sha256 value, I first supplied a garbage one
+      # and ran `nix-build -L` in the folder containing this file.
+      # The resulting error suggested this one.
+  	fetchSubmodules = true;
+  };
+
+  buildInputs = [ cmake
+                  supercollider
+                  fftw
+                  libsndfile ];
+
+  cmakeFlags = [
+    "-DSUPERNOVA=OFF"
+	  "-DSC_PATH=${supercollider}/include/SuperCollider"
+	  "-DFFTW3F_LIBRARY=${fftw}/lib/"];
+}
