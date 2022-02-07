a7fdd28e36f9f6c685e74463d48f9a057683435b
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-02-07 13:45:41 -0500

    fix gnupg

--- config/configuration.nix
+++ config/configuration.nix
@@ -105,10 +105,13 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "gtk2"; # https://discourse.nixos.org/t/cant-get-gnupg-to-work-no-pinentry/15373/2
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
