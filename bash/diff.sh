79d4ca27c8067f2abfbf4de3f89a4fec3c1984ba
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-05-09 15:22:50 -0500

    add .gitconfig
    update bash/diff so that it doesn't use diff-so-fancy

--- bash/diff.sh
+++ bash/diff.sh
@@ -1,7 +1,9 @@
 for i in `cat config/nix-files-to-copy.txt`; do
     echo ""
     echo "----"$i":"
-    diff -u /etc/nixos/$i config/$i | diff-so-fancy
+    diff -u /etc/nixos/$i config/$i # | diff-so-fancy
+      # diff-so-fancy is not available even after this minimal install,
+      # let alone before.
 done
 
 echo ""
