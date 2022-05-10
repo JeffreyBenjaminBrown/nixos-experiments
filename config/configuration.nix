9bd8725fed876c8c5cdf6b2328aa7234d0778135
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-05-09 19:34:21 -0500

    making an SSH server, maybe

--- config/configuration.nix
+++ config/configuration.nix
@@ -117,10 +117,13 @@
   };
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = true;
+  };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
