ea4e11561055e6561e552efc5d8c61b31c292970
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-04-27 17:26:48 -0500

    "merge" (by hand) yet more changes from
      system-hp17-unstable-post-21.11

--- config/configuration.nix
+++ config/configuration.nix
@@ -120,7 +120,7 @@
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix
+  # (e.g. `man configuration.nix`
   # or on https://nixos.org/nixos/options.html).
   #   TODO : fill in this line from nixos-generate-config
   #   system.stateVersion = "xx.xx";
