c9fddb57acd3dc59eca421eea43cd12b1d886c22
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-12-22 17:20:44 -0500

    bash/rebuild no longer uses git checkout as superuser
      That was problematic, because it kept changing the user
      of the files it checked out from jeff to root.

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -1,4 +1,5 @@
-git checkout kde
-nixos-rebuild switch --profile-name KDE
+sudo echo "Some of these commands need sudo."
 git checkout xfce
-nixos-rebuild switch --profile-name Xfce
+sudo nixos-rebuild switch --profile-name "Xfce . . . Xfce . . . Xfce"
+git checkout kde
+sudo nixos-rebuild switch --profile-name "KDE === KDE === KDE"
