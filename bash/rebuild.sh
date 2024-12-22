87a1743e856abf1605f6da4c2444ff09b6274d62
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-12-22 09:39:05 -0500

    simplify build names

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -1,4 +1,4 @@
 git checkout kde
-nixos-rebuild switch --profile-name KDE.buffer_128
+nixos-rebuild switch --profile-name KDE
 git checkout xfce
-nixos-rebuild switch --profile-name XFCE.buffer_128.no_pulseaudio
+nixos-rebuild switch --profile-name Xfce
