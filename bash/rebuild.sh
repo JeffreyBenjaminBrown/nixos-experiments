6f5510fa2736fb8409f63b9bf01d05d30829127a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2024-12-30 11:48:59 -0500

    bugfix
      After checking out a branch, before rebuilding NixOS,
      had forgotten to copy the config to /etc/nixos.

--- bash/rebuild.sh
+++ bash/rebuild.sh
@@ -2,11 +2,13 @@ echo "Some of these commands need sudo."
 sudo echo "Apparently you can sudo."
 
 git checkout xfce
+sudo ./bash/copy.sh
 # PITFALL: Profile names should not contain spaces.
 # (If they do, deleting them becomes difficult.)
 sudo nixos-rebuild switch --profile-name "Xfce___l___l___l___Xfce"
 
 git checkout kde
+sudo ./bash/copy.sh
 # PITFALL: Profile names should not contain spaces.
 # (If they do, deleting them becomes difficult.)
 sudo nixos-rebuild switch --profile-name "KDE________________KDE"
