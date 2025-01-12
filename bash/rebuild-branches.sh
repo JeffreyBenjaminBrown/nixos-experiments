924b9bf26a3ad472d4d492497f4bf33731e261a7
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-01-12 17:42:51 -0500

    more

--- bash/rebuild-branches.sh
+++ bash/rebuild-branches.sh
@@ -1,14 +1,12 @@
-echo "Some of these commands need sudo."
-sudo echo "Apparently you can sudo."
+# PITFALL: Profile names should not contain spaces.
+# (If they do, deleting them becomes difficult.)
+
+sudo echo ""
 
 git checkout xfce
 sudo ./bash/copy.sh
-# PITFALL: Profile names should not contain spaces.
-# (If they do, deleting them becomes difficult.)
 sudo nixos-rebuild switch --profile-name "Xfce___l___l___l___Xfce"
 
 git checkout kde
 sudo ./bash/copy.sh
-# PITFALL: Profile names should not contain spaces.
-# (If they do, deleting them becomes difficult.)
 sudo nixos-rebuild switch --profile-name "KDE________________KDE"
