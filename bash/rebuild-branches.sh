0b86ddb910423de0930752abed870d4364362b81
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-08-17 11:50:53 -0500

    factor out a function

--- bash/rebuild-branches.sh
+++ bash/rebuild-branches.sh
@@ -1,20 +1,22 @@
 # PITFALL: Profile names should not contain spaces.
 # (If they do, deleting them becomes difficult.)
 
+rebuild_branch () {
+    local branch="$1"
+    local profile_name="$2"
+
+    git checkout "$branch"
+    sudo ./bash/copy.sh
+    sudo nixos-rebuild switch \
+         --keep-failed \
+         --profile-name "$profile_name"
+}
+
 sudo nix-channel --update nixos
 cd ~/nix/jbb/
 echo ""
 
-git checkout xfce
-sudo ./bash/copy.sh
-sudo nixos-rebuild switch \
-     --keep-failed        \
-     --profile-name "Xfce___l___l___l___Xfce"
-
-git checkout kde
-sudo ./bash/copy.sh
-sudo nixos-rebuild switch \
-     --keep-failed        \
-     --profile-name "KDE________________KDE"
+rebuild_branch "xfce" "Xfce____llllll____Xfce"
+rebuild_branch "kde"  "KDE________________KDE"
 
 git push origin kde xfce
