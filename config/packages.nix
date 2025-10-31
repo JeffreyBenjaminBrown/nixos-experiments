e6ca0d4349a7fb5c8b2dee17b7ac29fa669332b7
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2025-10-31 09:08:24 -0500

    add a package

--- config/packages.nix
+++ config/packages.nix
@@ -282,6 +282,7 @@
         name = "jbb-plugins";
         paths = [
           drumgizmo
+          ffmpeg
 
           #### Below:                                       ########
           ####   audio, maybe gratuitous,                   ########
